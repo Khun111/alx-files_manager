2e4c23cacbf9d6ca362d97999bbcb17c8aefad46
Alvin <alvinvaati@gmail.com>
2023-09-28 23:40:51 +0300

    lint fix

--- utils/redis.js
+++ utils/redis.js
@@ -3,28 +3,29 @@ import { promisify } from 'util';
 const redis = require('redis');
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('error', (error) => console.error(error));
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => console.error(error));
+  }
 
-    isAlive() {
-        return this.client.connected ? True : false;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        const value = await promisify(this.client.get).bind(this.client)(key);
-        return value;
-    }
+  async get(key) {
+    const value = await promisify(this.client.get).bind(this.client)(key);
+    return value;
+  }
 
-    async set(key, value, duration) {
-        await promisify(this.client.set).bind(this.client)(key, value, 'EX', duration);
-    }
+  async set(key, value, duration) {
+    await promisify(this.client.set).bind(this.client)(key, value, 'EX', duration);
+  }
 
-    async del(key) {
-        await promisify(this.client.del).bind(this.client)(key);
-    }
+  async del(key) {
+    await promisify(this.client.del).bind(this.client)(key);
+  }
 }
 
 const redisClient = new RedisClient();
 export default redisClient;
+module.exports = redisClient;
