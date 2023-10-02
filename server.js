ddef7a35c53f0f2506c959e2a78c99e7dcaefef6
Alvin <alvinvaati@gmail.com>
2023-10-02 12:13:34 +0300

    server

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-#!/usr/bin/node
-const express = require('express');
-const routes = require('./routes/index');
+import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(express.json());
+
+app.use(routes);
 const port = process.env.PORT || 5000;
 
-app.use(express.json({ limit: '50mb' }));
-
-app.use('/', routes);
-app.listen(port, () => { console.log(`Server is running on port ${port}`); });
+app.listen(port, () => console.log(`Server running on ${port}`));
