6f81d4fb64e9f8b2946147fe60d3f5c86a0323c1
Alvin <alvinvaati@gmail.com>
2023-10-04 23:03:25 +0300

    status

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,8 +3,8 @@ import dbClient from '../utils/db';
 
 class AppController {
   static async  getStatus(_, res) {
-    const red = redisClient.isAlive();
-    const db = dbClient.isAlive();
+    const red = await redisClient.isAlive();
+    const db = await dbClient.isAlive();
     return res.status(200).json({ redis: red, db: db });
   }
 
